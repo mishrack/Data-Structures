ba3ae7260d4df030aaf3f054be0e0587464111ce
ck_mishra <35187244+chanchal4u@users.noreply.github.com>
2021-10-30 16:19:26 +0530

    Adding Node class java <30-Oct-21>

---
+++ Node.java
@@ -0,0 +1,8 @@
+package linkedlist;
+
+// Creating node class to define Node object for singly Linkedlist
+public class Node
+{
+	int data  ;   // value of node
+	Node next ;   // reference of next node
+}
